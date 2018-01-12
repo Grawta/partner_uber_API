9738652956db5e6ff7b01cf9ca660b618c107002
Grawta <jeanmatt@laposte.net>
2018-01-12 17:35:56 +0100

    Add json gem to populate database with json data, change seed to do a run on heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-records = JSON.parse(File.read('./data.json'))
+records = JSON.parse(File.read("data.json"))
 records.each do |record|
 	UberPickup.create(record)
 end
